408dafb72cdd389119fbc11775a4327071cd2de8
gisellevonbingen <gisellevonbingen@gmail.com>
2022-11-09 00:22:27 +0900

    Implement mp4, x-mpegURL download

---
+++ DownloadData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace TwitterVideoDownloader
+{
+    public class DownloadData
+    {
+        public Size Size { get; set; }
+        public List<Uri> Segments { get; } = new List<Uri>();
+
+        public DownloadData()
+        {
+
+        }
+
+        public DownloadData(params Uri[] segments)
+        {
+            this.Segments.AddRange(segments);
+        }
+
+        public DownloadData(IEnumerable<Uri> segments)
+        {
+            this.Segments.AddRange(segments);
+        }
+
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -15,12 +16,20 @@ namespace TwitterVideoDownloader
 {
     public class Program
     {
-        public static Regex TweetStatusPattern { get; } = new Regex("https:\\/\\/twitter\\.com\\/(?<user_id>.+)\\/status\\/(?<tweet_id>.+)");
+        public static string TweetIdGroup { get; } = "tweet_id";
+        public static Regex TweetStatusPattern { get; } = new Regex($"https:\\/\\/twitter\\.com\\/(?<user_id>.+)\\/status\\/(?<{TweetIdGroup}>.+)");
         public static Regex TweetDetailPattern { get; } = new Regex("https:\\/\\/twitter\\.com\\/i\\/api\\/graphql\\/\\w+\\/TweetDetail");
         public static Regex VidPattern { get; } = new Regex(".+/vid\\/(?<size>\\w+)\\/.*");
+        public static Regex PlPattern { get; } = new Regex(".+/pl\\/(?<size>\\w+)\\/.*");
+        public static string MapUriPrefix { get; } = "#EXT-X-MAP:URI";
+        public static string OutputDirectory { get; } = "./Output";
+        public static int DownloadBufferSize { get; } = 16 * 1024;
 
         public static void Main()
         {
+            var outputDirectory = Directory.CreateDirectory(OutputDirectory);
+            outputDirectory.Create();
+
             var mediaList = new List<TwitterExtendedMediaEntity>();
             using var are = new AutoResetEvent(false);
 
@@ -47,11 +56,16 @@ namespace TwitterVideoDownloader
                 {
                     break;
                 }
-                else if (TweetStatusPattern.IsMatch(url) == false)
+
+                var statusMatch = TweetStatusPattern.Match(url);
+
+                if (statusMatch.Success == false)
                 {
                     Console.WriteLine("Wrong tweet url");
                     continue;
                 }
+
+                var tweetId = statusMatch.Groups[TweetIdGroup].Value;
 
                 mediaList.Clear();
                 driver.Navigate().GoToUrl(url);
@@ -66,54 +80,132 @@ namespace TwitterVideoDownloader
                     Console.WriteLine($"Media Found : {mediaList.Count}");
                     Console.WriteLine();
 
+                    var downladIndex = 0;
+
                     for (var i = 0; i < mediaList.Count; i++)
                     {
                         var media = mediaList[i];
                         var variants = media.VideoInfo.Variants;
-                        Console.WriteLine($"==================== Media {i + 1}/{mediaList.Count}");
+                        var downloadList = variants.SelectMany(v => GetDownloadDataList(v)).ToArray();
+
+                        Console.WriteLine($"{new string('=', 20)} Media {i + 1}/{mediaList.Count}");
                         Console.WriteLine($"Thumbnail: {media.Thumbnail}");
                         Console.WriteLine($"Duration : {media.VideoInfo.Duration} ms");
                         Console.WriteLine($"Variants Found : {variants.Count}");
+                        Console.WriteLine($"Downloads Found : {downloadList.Length}");
                         Console.WriteLine();
 
-                        for (int j = 0; j < variants.Count; j++)
+                        for (var k = 0; k < downloadList.Length; k++)
                         {
-                            var variant = variants[j];
-                            Console.WriteLine($"========== Varient {j + 1}/{variants.Count}");
-                            Console.WriteLine($"Url: {variant.Url}");
+                            var download = downloadList[k];
 
-                            if (variant.ContentType.Equals("video/mp4") == true)
+                            if (download.Segments.Count == 0)
                             {
-                                var size = VidPattern.Match(variant.Url).Groups["size"];
-                                Console.WriteLine($"Size: {size.Value}");
-                            }
-                            else if (variant.ContentType.Equals("application/x-mpegURL") == true)
-                            {
-                                var http = WebRequest.CreateHttp(new Uri(variant.Url));
-                                http.Method = HttpMethod.Get.Method;
-
-                                using var response = http.GetResponse();
-                                using var responseReader = new StreamReader(response.GetResponseStream());
-                                var m3u8 = responseReader.ReadToEnd();
-                                Console.WriteLine();
-                                Console.WriteLine("===== start of m3u8");
-                                Console.WriteLine(m3u8);
-                                Console.WriteLine("===== end of m3u8");
-                            }
-                            else
-                            {
-                                Console.WriteLine($"Unknown ContentType: {variant.ContentType}");
+                                continue;
                             }
 
+                            var fileName = $"{tweetId}_{++downladIndex}_{download.Size.Width}x{download.Size.Height}_{Path.GetFileName(download.Segments[0].LocalPath)}";
+
+                            Console.WriteLine($"{new string('=', 10)} File {k + 1}/{downloadList.Length}");
+                            Console.WriteLine($"Size : {download.Size}");
+                            Console.WriteLine($"File Name : {fileName}");
                             Console.WriteLine();
+
+                            using var fs = new FileStream(Path.Combine(outputDirectory.FullName, fileName), FileMode.Create);
+
+                            foreach (var segment in download.Segments)
+                            {
+                                using var segmentResponse = WebRequest.CreateHttp(segment).GetResponse();
+                                using var segmentResponseStream = segmentResponse.GetResponseStream();
+                                segmentResponseStream.CopyTo(fs, DownloadBufferSize);
+                            }
+
                         }
 
+                        Console.WriteLine();
                     }
 
                 }
 
             }
 
+        }
+
+        private static IEnumerable<DownloadData> GetDownloadDataList(TwitterVideoVariant variant)
+        {
+            if (variant.ContentType.Equals("video/mp4") == true)
+            {
+                var size = ParseSize(VidPattern.Match(variant.Url).Groups["size"].Value);
+                yield return new DownloadData(new Uri(variant.Url)) { Size = size };
+            }
+            else if (variant.ContentType.Equals("application/x-mpegURL") == true)
+            {
+                var variantUri = new Uri(variant.Url);
+                using var response = WebRequest.CreateHttp(variantUri).GetResponse();
+                using var responseReader = new StreamReader(response.GetResponseStream());
+
+                for (string line = null; (line = responseReader.ReadLine()) != null;)
+                {
+                    if (line.StartsWith("#") == true)
+                    {
+                        continue;
+                    }
+
+                    var size = ParseSize(PlPattern.Match(line).Groups["size"].Value);
+                    var m3u8Uri = new Uri(variantUri, line);
+                    var segments = GetM3U8Segments(m3u8Uri).Select(s => new Uri(m3u8Uri, s));
+                    yield return new DownloadData(segments) { Size = size };
+                }
+
+            }
+            else
+            {
+                Console.WriteLine($"Unknown ContentType: {variant.ContentType}");
+            }
+
+        }
+
+        private static IEnumerable<string> GetM3U8Segments(Uri uri)
+        {
+            using var response = WebRequest.CreateHttp(uri).GetResponse();
+            using var responseReader = new StreamReader(response.GetResponseStream());
+
+            for (string line = null; (line = responseReader.ReadLine()) != null;)
+            {
+                if (line.StartsWith(MapUriPrefix) == true)
+                {
+                    //#EXT-X-MAP:URI=
+                    var mapUri = line[(MapUriPrefix.Length + 1)..];
+
+                    if (mapUri.StartsWith("\"") == true && mapUri.EndsWith("\"") == true)
+                    {
+                        yield return mapUri[1..^1];
+                    }
+                    else
+                    {
+                        yield return mapUri;
+                    }
+
+                }
+                else if (line.StartsWith("#") == true)
+                {
+                    continue;
+                }
+                else
+                {
+                    yield return line;
+                }
+
+            }
+
+        }
+
+        private static Size ParseSize(string text)
+        {
+            var splits = text.Split('x');
+            var width = int.Parse(splits[0]);
+            var height = int.Parse(splits[1]);
+            return new Size(width, height);
         }
 
         private static void OnNetworkResponseReceived(NetworkResponseReceivedEventArgs e, AutoResetEvent are, List<TwitterExtendedMediaEntity> list)
